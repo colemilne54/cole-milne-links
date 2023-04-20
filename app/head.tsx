4a312c915ea7bf796c2fe13c03362822e427374b
Cole Milne <colemilne54@gmail.com>
2023-04-20 16:16:34 -0600

    Icons working, onto side projects

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
     return (
       <>
-        <title>Linktree</title>
+        <title>Cole Milne Links</title>
         <meta name="description" content="Cole Milne Links" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
