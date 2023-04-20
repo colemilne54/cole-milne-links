b0fdbeccff6c068dd6da8f31cc275f339c773333
Cole Milne <colemilne54@gmail.com>
2023-04-20 16:44:20 -0600

    rebuild

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,18 @@
-import '../styles/globals.css';
-// import { Analytics } from '@vercel/analytics/react';
+import './globals.css'
+
+export const metadata = {
+  title: 'Cole Milne Links',
+  description: "Links for Cole Milne's software development projects",
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
